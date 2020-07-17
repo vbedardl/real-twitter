ba196727a9ec1aecacf4266835dbb88e47290bc3
Will <mack.mindenhall@gmail.com>
2020-07-17 16:02:19 -0400

    Basic routing to single tweet page

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,46 +1,50 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
-const express       = require('express');
-const router  = express.Router();
-const { users } = require('../data-files/usersDB')
-const db = require('../lib/in-memory-db')
-const TwitterObj = require('../schema/Tweet')
-const { getTweetById } = require('../lib/util/helper')
-
+const userHelper = require("../lib/util/user-helper");
+const express = require("express");
+const router = express.Router();
+const { users } = require("../data-files/usersDB");
+const db = require("../lib/in-memory-db");
+const TwitterObj = require("../schema/Tweet");
+const { getTweetById } = require("../lib/util/helper");
 
 //GET LIST OF TWEETS
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
   const sortNewestFirst = (a, b) => a.created_at - b.created_at;
   const sortedTweets = db.tweets.sort(sortNewestFirst);
-  res.json(sortedTweets)
-})
+  res.json(sortedTweets);
+});
 
 //POST A NEW TWEET
-router.post('/', (req, res) => {
-  if(!req.body.text){
-    res.status(400).json({ error: 'invalid request: no data in POST body'})
-    return
+router.post("/", (req, res) => {
+  if (!req.body.text) {
+    res.status(400).json({ error: "invalid request: no data in POST body" });
+    return;
   }
 
-    const user = users[req.session.user] ? users[req.session.user] : userHelper.generateRandomUser();
-    const theTweet = new TwitterObj(req.body.text, user)
-    const tweet = {
-      user: user,
-      content: theTweet,
-      created_at: Date.now()
-    };
-    db.tweets.push(tweet)
-    res.status(201).send();
-
-})
+  const user = users[req.session.user]
+    ? users[req.session.user]
+    : userHelper.generateRandomUser();
+  const theTweet = new TwitterObj(req.body.text, user);
+  const tweet = {
+    user: user,
+    content: theTweet,
+    created_at: Date.now(),
+  };
+  db.tweets.push(tweet);
+  res.status(201).send();
+});
 
 //GET A SPECIFIC TWEET BY ID
-router.get('/:id', (req, res) => {
-  const tweet = getTweetById(req.params, db)
+router.get("/:id", (req, res) => {
+  const user = users[req.session.user]
+    ? users[req.session.user]
+    : userHelper.generateRandomUser();
 
-  res.render('tweet_page', {tweet, tweet})
-})
+  console.log("body: ", req.session.user);
+  const tweet = getTweetById(req.params, db);
 
-module.exports = router
+  res.render("tweet_page", { user: user, tweet: tweet });
+});
+
+module.exports = router;
